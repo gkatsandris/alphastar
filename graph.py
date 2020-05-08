04e539d6cad6dce05c521eead618d3962af50fc2
georgios <george.katsandris@yandex.com>
2020-05-08 21:09:08 +0200

    Make graph.py more compact and remove example

--- graph.py
+++ graph.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-class AdjacentVertexListCell:
-    def __init__(self):
-        self.adjacent_vertex_id = None
-        self.next = None
+class AdjacencyListCell:
+    def __init__(self,adjacent_vertex_id,former_first_adjacent_cell):
+        self.vertex_id = adjacent_vertex_id
+        self.next = former_first_adjacent_cell
 
 class Graph:
     def __init__(self,vertex_number):
@@ -12,35 +12,16 @@ class Graph:
         
     def add_edge(self,src,dest):        
         #prepend dest's index to src's adjacency list
-        new_cell = AdjacentVertexListCell()
-        new_cell.adjacent_vertex_id = dest
-        new_cell.next = self.graph[src]
-        self.graph[src] = new_cell
+        self.graph[src] = AdjacencyListCell(dest,self.graph[src])
         
         #prepend src's index to dest's adjacency list (undirected graph is assumed)
-        new_cell = AdjacentVertexListCell()
-        new_cell.adjacent_vertex_id = src
-        new_cell.next = self.graph[dest]
-        self.graph[dest] = new_cell
+        self.graph[dest] = AdjacencyListCell(src,self.graph[dest])
         
     def print_graph(self): 
         for i in range(self.vertex_number): 
             print("Adjacency list of vertex {}\n head".format(i), end="") 
             temp = self.graph[i] 
             while temp: 
-                print(" -> {}".format(temp.adjacent_vertex_id), end="") 
+                print(" -> {}".format(temp.vertex_id), end="") 
                 temp = temp.next
-            print(" \n") 
-        
-if __name__ == "__main__": 
-    V = 5
-    graph = Graph(V) 
-    graph.add_edge(0, 1) 
-    graph.add_edge(0, 4) 
-    graph.add_edge(1, 2) 
-    graph.add_edge(1, 3) 
-    graph.add_edge(1, 4) 
-    graph.add_edge(2, 3) 
-    graph.add_edge(3, 4) 
-  
-    graph.print_graph() 
+            print(" \n")
