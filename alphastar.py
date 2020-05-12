08972cc47256cc147e0eebd78ae605dc4312927f
georgios <george.katsandris@yandex.com>
2020-05-12 16:10:00 +0200

    Change main code to use the new add_edge function requiring edge costs

--- alphastar.py
+++ alphastar.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 import graph
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     V = 5
-    g = graph.Graph(V) 
-    g.add_edge(0, 1) 
-    g.add_edge(0, 4) 
-    g.add_edge(1, 2) 
-    g.add_edge(1, 3) 
-    g.add_edge(1, 4) 
-    g.add_edge(2, 3) 
-    g.add_edge(3, 4) 
+    g = graph.Graph(V)
+    g.add_edge(0, 1, 1)
+    g.add_edge(0, 4, 1)
+    g.add_edge(1, 2, 1)
+    g.add_edge(1, 3, 1)
+    g.add_edge(1, 4, 1)
+    g.add_edge(2, 3, 1)
+    g.add_edge(3, 4, 1)
   
     g.print_graph() 
